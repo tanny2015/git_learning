5bea3db7ce3105ca26928cf0db4440f1e7da80f3
tanorgin <tanorigin@163.com>
2015-09-22 13:46:40 +0800

    Initial Commit

---
+++ git_learning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git_learning
+//
+//  Created by tanny on 15/9/22.
+//  Copyright © 2015年 tanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
